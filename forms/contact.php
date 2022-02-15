9a3d308259d6a2199c98be6936bb40754279efae
Ashraf Aiman <4shraf4iman@gmail.com>
2022-02-16 00:05:49 +0800

    Add all my files

--- forms/contact.php
+++ forms/contact.php
@@ -1,36 +1,45 @@
 <?php
-  
 
+#Receive user input
+$email_address = $_POST['email_address'];
+$feedback = $_POST['feedback'];
 
-  $receiving_email_address = '4shraf4iman@gmail.com';
+#Filter user input
+function filter_email_header($form_field) {  
+return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
+}
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$email_address  = filter_email_header($email_address);
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+#Send email
+$headers = "From: $email_addressn";
+$sent = mail('4shraf4iman@gmail.com', 'Feedback Form Submission', $feedback, $headers);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+#Thank user or notify them of a problem
+if ($sent) {
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+?><html>
+<head>
+<title>Thank You</title>
+</head>
+<body>
+<h1>Thank You</h1>
+<p>Thank you for your feedback.</p>
+</body>
+</html>
+<?php
 
-  echo $contact->send();
+} else {
+
+?><html>
+<head>
+<title>Something went wrong</title>
+</head>
+<body>
+<h1>Something went wrong</h1>
+<p>We could not send your feedback. Please try again.</p>
+</body>
+</html>
+<?php
+}
 ?>
